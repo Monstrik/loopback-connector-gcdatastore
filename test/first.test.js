080e60061b1f8594ef66e1816cf4fb44fc15a6ce
AlexYakovis <yakovis@boylesoftware.com>
2017-01-19 15:30:43 -0500

    first tests

--- test/first.test.js
+++ test/first.test.js
@@ -1,10 +1,9 @@
-
 require('./init.js');
 
 var ds = getDataSource();
 
-describe('test test framework', function() {
-  it('empty test', function(done) {
+describe('test test framework', function () {
+  it('empty test', function (done) {
     done();
   });
 });
@@ -15,9 +14,16 @@ describe('Create Model', function () {
     {name: String, emails: [String], age: Number},
     {forceId: false});
   before(function (done) {
-    //done();
-    Customer.deleteAll(done);
-    //Customer.create({name: 'TestName', emails: ['TestEmail'], age: 32},{},done)
+
+    Customer.all(function (err, result) {
+      var keyArr = [];
+      result.forEach(function (item) {
+        keyArr.push(item.id)
+      });
+      //console.log(keyArr);
+      done();
+    });
+
   });
 
   it('should create Model', function (done) {
@@ -25,46 +31,34 @@ describe('Create Model', function () {
       name: 'John1',
       emails: ['john@x.com', 'john@y.com'],
       age: 30,
-    }, function(err, customer) {
-      customer.name.should.equal('John1');
+    }, function (err, customer) {
+      //customer.name.should.equal('John1');
+      customer.should.have.property('id');
       done();
-      // Customer.create({
-      //   name: 'John2',
-      //   emails: ['john2@x.com', 'john2@y.com'],
-      //   age: 40,
-      // }, function(err, customer) {
-      //   customer.name.should.equal('John2');
-      //   done(err, customer);
-      // });
     });
-
   });
-//
-//   // it('should Save Model', function (done) {
-//   //   Customer.save({
-//   //     name: 'John1',
-//   //     emails: ['john@x.com', 'john@y.com'],
-//   //     age: 30,
-//   //   }, function(err, customer) {
-//   //     customer.save(done);
-//   //   });
-//   //
-//   // });
-//
-//
-//   //
-//   // it('should allow custom name for the id property for findById', function(done) {
-//   //   Customer.findById(1, function(err, customer) {
-//   //     customer.seq.should.equal(1);
-//   //     done(err, customer);
-//   //   });
-//   // });
-//   //
-//   // it('should allow inq with find', function(done) {
-//   //   Customer.find({ where: { seq: { inq: [1] }}}, function(err, customers) {
-//   //     customers.length.should.equal(1);
-//   //     customers[0].seq.should.equal(1);
-//   //     done(err);
-//   //   });
-//   // });
- });
+
+  it('should create and delete Model', function (done) {
+    Customer.create({
+      name: 'John1',
+      emails: ['john@x.com', 'john@y.com'],
+      age: 30,
+    }, function (err, customer) {
+      customer.should.have.property('id');
+      var  tid= customer.id;
+      console.log(tid);
+      Customer.destroyById(tid*1, function(err,data){
+        Customer.findById(tid, function (err, found) {
+          if (err) {
+            console.error(err);
+          }
+          else {
+            console.error(found);
+            done();
+          }
+        })
+
+      });
+    });
+  });
+});
